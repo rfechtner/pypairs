eb7370ed7399389bca09718abc35a0ea260d0814
Ron Fechtner <fechtner@gitlab.cip.ifi.lmu.de>
2018-02-21 23:39:19 +0100

    Added setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pypairs',
+  packages = ['pypairs'],
+  version = '0.1',
+  description = 'Reimplementation of the Pairs method described by A. Scialdone et al.',
+  author = 'Ron Fechtner',
+  author_email = 'ronfechtner@gmail.com',
+  url = 'https://github.com/rfechtner/pypairs',
+  download_url = 'https://github.com/rfechtner/pypairs/archive/0.1.tar.gz',
+  keywords = ['scRNA', 'scanpy', 'pairs'],
+  classifiers = [],
+)
