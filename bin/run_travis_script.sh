75773142c15e62b2e42a0afffd3f99078206267f
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 14:03:13 +0100

    Update run_travis_script.sh

--- bin/run_travis_script.sh
+++ bin/run_travis_script.sh
@@ -3,6 +3,7 @@
 set -ev
 
 if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
+  pip freeze
   pytest --cov=./
   rst2html.py --halt=2 README.rst >/dev/null
   export PRERELEASE=`echo $TRAVIS_TAG | grep -c "b"`
