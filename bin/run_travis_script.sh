394513eeb548c8788ab393218f8f2ee24000cdfd
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 17:09:05 +0100

    Update run_travis_script.sh

--- bin/run_travis_script.sh
+++ bin/run_travis_script.sh
@@ -8,5 +8,5 @@ if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
   rst2html.py --halt=2 README.rst >/dev/null
   export PRERELEASE=`echo $TRAVIS_TAG | grep -c "b"`
 elif [ "${SYSTEM}" = "scientific" ]; then
-  docker exec sl_test python -m pytest
+  docker exec -it sl_test bash -c 'source /root/miniconda/bin/activate pypairs_test_env && cd /root/pypairs/ && pytest'
 fi
