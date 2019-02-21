36d1e0d5f1bb2c80005ca76fc894e209af32b9c9
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 12:55:06 +0100

    Update run_travis_before_install.sh

--- bin/run_travis_before_install.sh
+++ bin/run_travis_before_install.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
 set -ev
-echo "Running before install"
-if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
-  echo "Installing Conda"
-  ./bin/install_conda.sh
-  echo "Cleaning Cache"
-  rm -rf ./cache/ || true
-  mkdir ./cache/
-  rm -rf ./write/ || true
-  mkdir ./write/
-  rm -rf ./figures/ || true
-  mkdir ./figures
-elif [ "${SYSTEM}" = "scientific" ]; then
+if [ "${SYSTEM}" = "scientific" ]; then
   echo "Setting up scientifix linux docker container"
   docker pull scientificlinux/sl
   docker run -d -p 127.0.0.1:80:80 --name sl_test scientificlinux/sl:7
+  docker ps | grep -q sl_test
   cat /etc/os-release
-  sudo yum -y update
-  sudo yum install -y python36 python36-setuptools
-  sudo easy_install-3.6 pip
+  docker exec sl_test cat /etc/os-release
+  docker exec sl_test sudo yum -y update
+  docker exec sl_test sudo yum install -y python36 python36-setuptools
+  docker exec sl_test sudo easy_install-3.6 pip
 fi
