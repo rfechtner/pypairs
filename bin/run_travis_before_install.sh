45df929aa571b9b1817312c854688a69a0992dec
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 13:46:54 +0100

    Update run_travis_before_install.sh

--- bin/run_travis_before_install.sh
+++ bin/run_travis_before_install.sh
@@ -14,8 +14,9 @@ if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
 elif [ "${SYSTEM}" = "scientific" ]; then
   echo "Setting up scientifix linux docker container"
   docker pull scientificlinux/sl
-  docker run -d --name sl_test scientificlinux/sl:7
-  docker exec -i -t sl_test cat /etc/*-release
+  docker run -it -d --name sl_test scientificlinux/sl:7
+  docker ps -a
+  docker exec -it sl_test cat /etc/*-release
   docker exec -i -t sl_test sudo yum -y update
   docker exec -i -t sl_test sudo yum install -y python36 python36-setuptools
   docker exec -i -t sl_test sudo easy_install-3.6 pip
