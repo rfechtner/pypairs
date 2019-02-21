d789115f80f05e342d21ad08a94d1036300d3293
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 14:02:15 +0100

    Update run_travis_before_install.sh

--- bin/run_travis_before_install.sh
+++ bin/run_travis_before_install.sh
@@ -4,13 +4,6 @@ echo "Running before install"
 if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
   echo "Installing Conda"
   ./bin/install_conda.sh
-  echo "Cleaning Cache"
-  rm -rf ./cache/ || true
-  mkdir ./cache/
-  rm -rf ./write/ || true
-  mkdir ./write/
-  rm -rf ./figures/ || true
-  mkdir ./figures
 elif [ "${SYSTEM}" = "scientific" ]; then
   echo "Setting up scientifix linux docker container"
   docker pull scientificlinux/sl
@@ -18,7 +11,7 @@ elif [ "${SYSTEM}" = "scientific" ]; then
   docker ps -a
   docker exec -it sl_test cat /etc/os-release
   docker exec -it sl_test yum -y update
-  docker exec -it sl_test yum install -y python36 python36-setuptools
-  docker exec -it sl_test easy_install-3.6 pip
+  docker exec -it sl_test yum install -y python3 python3-setuptools
+  docker exec -it sl_test easy_install pip
   docker exec -it sl_test git clone --depth=50 --branch=$TRAVIS_BRANCH https://github.com/rfechtner/pypairs.git rfechtner/pypairs
 fi
