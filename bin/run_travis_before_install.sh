dba504c24be991ea265dfcdfaf826208580e1db8
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 16:26:32 +0100

    Update run_travis_before_install.sh

--- bin/run_travis_before_install.sh
+++ bin/run_travis_before_install.sh
@@ -5,21 +5,22 @@ if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
   echo "Installing Conda"
   ./bin/install_conda.sh
 elif [ "${SYSTEM}" = "scientific" ]; then
-  echo "Setting up scientifix linux docker container"
+  # Setting up scientifix linux docker container
   docker pull scientificlinux/sl
   docker run -it -d --name sl_test --privileged=true scientificlinux/sl:7
   docker ps -a
+  # Verify os
   docker exec -it sl_test cat /etc/os-release
+  # Install miniconda
   docker exec -it sl_test yum -y update
   docker exec -it sl_test yum -y install wget
-  docker exec -it sl_test bash -c 'mkdir -p "$HOME"/download/'
-  docker exec -it sl_test bash -c 'wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$HOME"/download/miniconda.sh'
-  docker exec -it sl_test bash -c 'chmod +x "$HOME"/download/miniconda.sh'
-  docker exec -it sl_test bash -c '"$HOME"/download/miniconda.sh -b -p "$HOME"/miniconda'
-  docker exec -it sl_test bash -c 'alias python="$HOME"/miniconda/bin/python3'
-  docker exec -it sl_test bash -c 'which python'
+  docker exec -it sl_test bash -c 'mkdir -p /root/download/'
+  docker exec -it sl_test bash -c 'wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /root/download/miniconda.sh'
+  docker exec -it sl_test bash -c 'chmod +x /root/download/miniconda.sh'
+  docker exec -it sl_test bash -c '/root/download/miniconda.sh -b -p /root/miniconda'
+  docker exec -it sl_test alias python='/root/miniconda/bin/python3'
   docker exec -it sl_test python -V
-  ls -la
-  docker cp ./. sl_test:~/pypairs/
-  docker exec -it sl_test cd ~/pypairs/
+  # Copy repo
+  docker cp ./. sl_test:/root/pypairs/
+  docker exec -it sl_test cd /root/pypairs/
 fi
