41cfaf8e46d4ca7b3bac980ecd23ca833252dc99
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 14:45:55 +0100

    Update run_travis_before_install.sh

--- bin/run_travis_before_install.sh
+++ bin/run_travis_before_install.sh
@@ -11,7 +11,7 @@ elif [ "${SYSTEM}" = "scientific" ]; then
   docker ps -a
   docker exec -it sl_test cat /etc/os-release
   docker exec -it sl_test yum -y update
-  docker exec -it sl_test yum install wget
+  docker exec -it sl_test yum -y install wget
   docker exec -it sl_test bash -c 'mkdir -p "$HOME"/download/'
   docker exec -it sl_test bash -c 'wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$HOME"/download/miniconda.sh'
   docker exec -it sl_test bash -c '"$HOME"/download/miniconda.sh -b -p "$HOME"/miniconda'
