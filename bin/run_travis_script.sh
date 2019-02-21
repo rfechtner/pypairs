e18ef6d05cb94ea2cc1b9c99b7aeac81c4f4673f
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 18:27:25 +0100

    Update run_travis_script.sh

--- bin/run_travis_script.sh
+++ bin/run_travis_script.sh
@@ -3,10 +3,10 @@
 set -ev
 
 if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
-  pip freeze
   pytest --cov=./
   rst2html.py --halt=2 README.rst >/dev/null
-  export PRERELEASE=`echo $TRAVIS_TAG | grep -c "b"`
 elif [ "${SYSTEM}" = "scientific" ]; then
   docker exec -it sl_test bash -c 'source /root/miniconda/bin/activate pypairs_test_env && cd /root/pypairs/ && pytest'
 fi
+
+export PRERELEASE=`echo $TRAVIS_TAG | grep -c "b"`
