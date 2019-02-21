9c866347a75a5a5e9921491eba57f1585e908098
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 11:33:46 +0100

    Update run_travis_script.sh

--- bin/run_travis_script.sh
+++ bin/run_travis_script.sh
@@ -3,15 +3,9 @@
 set -ev
 
 if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
-
   pytest --cov=./
-  
-  if [ "${SYSTEM}" = "linux" ]; then 
-  
-    rst2html.py --halt=2 README.rst >/dev/null
-    export PRERELEASE=`echo $TRAVIS_TAG | grep -c "b"`
-    
-  fi
+  rst2html.py --halt=2 README.rst >/dev/null
+  export PRERELEASE=`echo $TRAVIS_TAG | grep -c "b"`
 elif [ "${SYSTEM}" = "scientific" ]; then
   docker exec sl_test python -m pytest
 fi
