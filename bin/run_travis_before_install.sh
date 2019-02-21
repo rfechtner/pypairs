009b02e17418c2ffe07c2b7accab6ffcc231d5e4
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 14:54:57 +0100

    Update run_travis_before_install.sh

--- bin/run_travis_before_install.sh
+++ bin/run_travis_before_install.sh
@@ -14,6 +14,7 @@ elif [ "${SYSTEM}" = "scientific" ]; then
   docker exec -it sl_test yum -y install wget
   docker exec -it sl_test bash -c 'mkdir -p "$HOME"/download/'
   docker exec -it sl_test bash -c 'wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$HOME"/download/miniconda.sh'
+  docker exec -it sl_test bash -c 'chmod +x "$HOME"/download/miniconda.sh'
   docker exec -it sl_test bash -c '"$HOME"/download/miniconda.sh -b -p "$HOME"/miniconda'
   docker exec -it sl_test python -V
   docker exec -it sl_test git clone --depth=50 --branch=$TRAVIS_BRANCH https://github.com/rfechtner/pypairs.git rfechtner/pypairs
