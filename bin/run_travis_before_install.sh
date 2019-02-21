5ae275b30194ef84546c0af7fcefc026bd407b87
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 14:15:21 +0100

    Update run_travis_before_install.sh

--- bin/run_travis_before_install.sh
+++ bin/run_travis_before_install.sh
@@ -11,7 +11,11 @@ elif [ "${SYSTEM}" = "scientific" ]; then
   docker ps -a
   docker exec -it sl_test cat /etc/os-release
   docker exec -it sl_test yum -y update
-  docker exec -it sl_test yum install -y python3 python3-setuptools
-  docker exec -it sl_test easy_install pip
+  docker exec -it sl_test subscription-manager repos --enable rhel-7-server-optional-rpms --enable rhel-server-rhscl-7-rpms
+  docker exec -it sl_test yum -y install @development
+  docker exec -it sl_test yum -y install rh-python36
+  docker exec -it sl_test yum -y install rh-python36-numpy rh-python36-scipy rh-python36-python-tools
+  docker exec -it sl_test scl enable rh-python36 bash
+  docker exec -it sl_test python3 -V
   docker exec -it sl_test git clone --depth=50 --branch=$TRAVIS_BRANCH https://github.com/rfechtner/pypairs.git rfechtner/pypairs
 fi
