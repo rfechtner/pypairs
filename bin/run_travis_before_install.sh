765ccaaa37c55c02b914eda5c8848829e0173cc0
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 11:46:40 +0100

    Update run_travis_before_install.sh

--- bin/run_travis_before_install.sh
+++ bin/run_travis_before_install.sh
@@ -3,7 +3,7 @@
 set -ev
 
 if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
-  ./bin/install_conda.sh
+  ./install_conda.sh
   rm -rf ./cache/ || true
   mkdir ./cache/
   rm -rf ./write/ || true
