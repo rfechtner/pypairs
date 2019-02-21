dd21b8ada5989653da76d04e00e0d8f42afe45c7
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 13:55:55 +0100

    Update run_travis_install.sh

--- bin/run_travis_install.sh
+++ bin/run_travis_install.sh
@@ -5,7 +5,7 @@ set -ev
 if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
   source $HOME/miniconda/bin/activate
   conda install python=3.6 numpy numba pandas scikit-learn colorama Sphinx pytest pytest-cov docutils pytables -y
-  pip install sphinx-autodoc-typehints sphinx-rtd-theme codecov
+  pip install sphinx-autodoc-typehints sphinx-rtd-theme codecov pytest
   pip install -e .[plotting]
 elif [ "${SYSTEM}" = "scientific" ]; then
   docker exec sl_test pip install -m Sphinx pytest pytest-cov docutils sphinx-autodoc-typehints sphinx-rtd-theme codecov
