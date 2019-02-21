c3e02371906343f5336004b92da289a8ceb6047f
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 18:54:37 +0100

    Update run_travis_install.sh

--- bin/run_travis_install.sh
+++ bin/run_travis_install.sh
@@ -4,7 +4,7 @@ set -ev
 
 if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
   source $HOME/miniconda/bin/activate pypairs_test_env
-  pip install -r requirements
+  pip install -r requirements.txt
   pip install -e .[plotting]
 elif [ "${SYSTEM}" = "scientific" ]; then
   docker exec -it sl_test bash -c 'source /root/miniconda/bin/activate pypairs_test_env && cd /root/pypairs/ && pip install -r requirements.txt'
