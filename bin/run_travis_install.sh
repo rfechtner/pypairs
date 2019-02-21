dba67c8a649cd8fb7ecbe8326a7765965b09e5c1
Ron Fechtner <ronfechtner@gmail.com>
2019-02-21 18:27:01 +0100

    Update run_travis_install.sh

--- bin/run_travis_install.sh
+++ bin/run_travis_install.sh
@@ -3,9 +3,9 @@
 set -ev
 
 if [ "${SYSTEM}" = "linux" ] || [ "${SYSTEM}" = "osx" ]; then
-  source $HOME/miniconda/bin/activate
-  conda install python=3.6 numpy numba pandas scikit-learn colorama Sphinx pytest pytest-cov docutils pytables -y
-  pip install sphinx-autodoc-typehints sphinx-rtd-theme codecov pytest
+  source activate pypairs_test_env
+  pip install -y Sphinx pytest pytest-cov docutils sphinx-autodoc-typehints sphinx-rtd-theme codecov
+  pip install -r requirements
   pip install -e .[plotting]
 elif [ "${SYSTEM}" = "scientific" ]; then
   docker exec -it sl_test bash -c 'source /root/miniconda/bin/activate pypairs_test_env && cd /root/pypairs/ && pip install -r requirements.txt'
