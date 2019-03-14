58e340fbcca4345f2216c3efc4979de0cb9b4ba6
Ron Fechtner <ronfechtner@gmail.com>
2019-03-14 13:22:16 +0100

    Create test_datasets.py

---
+++ pypairs/tests/test_datasets.py
@@ -0,0 +1,16 @@
+from pypairs import datasets
+
+
+def test_leng15():
+    training_data = datasets.leng15(mode='all')
+
+    if not training_data.shape == (460, 19084):
+        raise AssertionError()
+
+    training_data = datasets.leng15(mode='sorted')
+    if not training_data.shape == (212, 19084):
+        raise AssertionError()
+
+    training_data = datasets.leng15(mode='unsorted')
+    if not training_data.shape == (247, 19084):
+        raise AssertionError()
