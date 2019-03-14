20f7e00d6168bb74071968394b4eae9471b2b087
Ron Fechtner <ronfechtner@gmail.com>
2019-03-14 13:48:56 +0100

    Update test_datasets.py

--- pypairs/tests/test_datasets.py
+++ pypairs/tests/test_datasets.py
@@ -8,9 +8,9 @@ def test_leng15():
         raise AssertionError()
 
     training_data = datasets.leng15(mode='sorted')
-    if not training_data.shape == (212, 19084):
+    if not training_data.shape == (247, 19084):
         raise AssertionError()
 
     training_data = datasets.leng15(mode='unsorted')
-    if not training_data.shape == (247, 19084):
+    if not training_data.shape == (213, 19084):
         raise AssertionError()
