b0db64156ec83823cb7cf4216b63c0fe5b1e7f25
Ron Fechtner <ron.fechtner@eqs.com>
2019-03-27 13:57:01 +0100

    added minimal test

--- pypairs/tests/test_sandbag.py
+++ pypairs/tests/test_sandbag.py
@@ -1,5 +1,6 @@
 from pypairs import datasets, settings, utils, pairs
 from pandas import DataFrame
+import numpy as np
 
 settings.verbosity = 4
 
@@ -21,6 +22,58 @@ ref_markers = {
     "G2M": [
         ("KPNA2","H2AFZ"),("RPS6","H2AFZ")
     ]}
+
+min_ref = {
+    'G1': [
+        ('0', '2'), ('0', '7'), ('10', '3'), ('5', '4'), ('8', '4'),
+        ('5', '7'), ('8', '7'), ('10', '7'), ('8', '9')],
+    'S': [
+        ('3', '0'), ('5', '0'), ('3', '2'), ('4', '2'), ('7', '2'),
+        ('3', '8'), ('3', '9'), ('5', '8'), ('6', '8')],
+    'G2M': [
+        ('0', '10'), ('2', '10'), ('4', '10'), ('9', '5'), ('6', '10'),
+        ('8', '10'), ('9', '10')]
+}
+
+
+def test_sandbag_min():
+    import string
+
+    data = np.array([
+        [48, 10, 63, 30, 1, 8, 4, 15, 20, 50, 20, 18],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [12, 42, 26, 8, 12, 1, 8, 38, 20, 15, 61, 52],
+        [22, 8, 8, 4, 43, 33, 21, 3, 1, 6, 4, 6],
+        [1, 41, 6, 5, 15, 12, 16, 8, 9, 3, 33, 29],
+        [30, 4, 41, 38, 3, 3, 8, 1, 0, 51, 6, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [2, 31, 6, 5, 31, 26, 29, 19, 28, 13, 29, 31],
+        [38, 12, 12, 43, 0, 18, 1, 4, 6, 51, 8, 9],
+        [10, 18, 18, 11, 6, 1, 5, 21, 5, 14, 18, 16],
+        [99, 1, 87, 66, 18, 19, 11, 2, 19, 75, 2, 4]
+    ])
+
+    data = data.T
+
+    cats = {
+        "G1": [0, 2, 3, 8],
+        "S": [4, 5, 6],
+        "G2M": [1, 7, 9, 10]
+    }
+
+    sample_names = list(string.ascii_uppercase[:12])
+    gene_names = list([str(x) for x in range(11)])
+
+    marker_pairs = pairs.sandbag(
+        data=data,
+        annotation=cats,
+        gene_names=gene_names,
+        sample_names=sample_names
+    )
+
+    print(marker_pairs)
+
+    assert utils.same_marker(marker_pairs, min_ref)
 
 
 def test_sandbag_1j():
