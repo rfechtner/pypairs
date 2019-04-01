c48b8bf16405d0c9c4b625d329419aa0f29b5479
Ron Fechtner <fechtner@gitlab.cip.ifi.lmu.de>
2019-04-01 17:30:49 +0200

    Refactored testing to be more precise

--- pypairs/tests/test_sandbag.py
+++ pypairs/tests/test_sandbag.py
@@ -1,6 +1,7 @@
 from pypairs import datasets, settings, utils, pairs
 from pandas import DataFrame
 import numpy as np
+from collections import Counter
 
 settings.verbosity = 4
 
@@ -23,158 +24,182 @@ ref_markers = {
         ("KPNA2","H2AFZ"),("RPS6","H2AFZ")
     ]}
 
+min_ref_mat = np.array([
+        [48, 10, 63, 30, 1, 8, 4, 15, 20, 50, 20, 18],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [12, 42, 26, 8, 12, 1, 8, 38, 20, 15, 61, 52],
+        [22, 8, 8, 4, 43, 33, 21, 3, 1, 6, 4, 6],
+        [1, 41, 6, 5, 15, 12, 16, 8, 9, 3, 33, 29],
+        [30, 4, 41, 38, 3, 3, 8, 1, 0, 51, 6, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [2, 31, 6, 5, 31, 26, 29, 19, 28, 13, 29, 31],
+        [38, 12, 12, 43, 0, 18, 1, 4, 6, 51, 8, 9],
+        [10, 18, 18, 11, 6, 1, 5, 21, 5, 14, 18, 16],
+        [99, 1, 87, 66, 18, 19, 11, 2, 19, 75, 2, 4]
+    ])
+
+min_ref_cats = {
+    "G1": [0, 2, 3, 8],
+    "S": [4, 5, 6],
+    "G2M": [1, 7, 9, 10]
+}
+
+min_ref_sample_names = ["A_G1", "B_G2M", "C_G1", "D_G1", "E_S", "F_S", "G_S", "H_G2M", "I_G1", "J_G2M", "K_G2M", "L_X"]
+min_ref_gene_names = list(["X{}".format(i) for i in range(0,11)])
+
 min_ref = {
     'G1': [
-        ('0', '2'), ('0', '7'), ('10', '3'), ('5', '4'), ('8', '4'),
-        ('5', '7'), ('8', '7'), ('10', '7'), ('8', '9')],
+        ('X0', 'X2'), ('X0', 'X7'), ('X10', 'X3'), ('X5', 'X4'), ('X8', 'X4'),
+        ('X5', 'X7'), ('X8', 'X7'), ('X10', 'X7'), ('X8', 'X9')],
     'S': [
-        ('3', '0'), ('5', '0'), ('3', '2'), ('4', '2'), ('7', '2'),
-        ('3', '8'), ('3', '9'), ('5', '8'), ('6', '8')],
+        ('X3', 'X0'), ('X5', 'X0'), ('X3', 'X2'), ('X4', 'X2'), ('X7', 'X2'),
+        ('X3', 'X8'), ('X3', 'X9'), ('X5', 'X8'), ('X6', 'X8')],
     'G2M': [
-        ('0', '10'), ('2', '10'), ('4', '10'), ('9', '5'), ('6', '10'),
-        ('8', '10'), ('9', '10')]
+        ('X0', 'X10'), ('X2', 'X10'), ('X4', 'X10'), ('X9', 'X5'), ('X6', 'X10'),
+        ('X8', 'X10'), ('X9', 'X10')]
 }
 
 
 def test_sandbag_min():
-    import string
+    print("")
+    print("")
 
-    data = np.array([
-        [48, 10, 63, 30, 1, 8, 4, 15, 20, 50, 20, 18],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [12, 42, 26, 8, 12, 1, 8, 38, 20, 15, 61, 52],
-        [22, 8, 8, 4, 43, 33, 21, 3, 1, 6, 4, 6],
-        [1, 41, 6, 5, 15, 12, 16, 8, 9, 3, 33, 29],
-        [30, 4, 41, 38, 3, 3, 8, 1, 0, 51, 6, 2],
-        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        [2, 31, 6, 5, 31, 26, 29, 19, 28, 13, 29, 31],
-        [38, 12, 12, 43, 0, 18, 1, 4, 6, 51, 8, 9],
-        [10, 18, 18, 11, 6, 1, 5, 21, 5, 14, 18, 16],
-        [99, 1, 87, 66, 18, 19, 11, 2, 19, 75, 2, 4]
-    ])
+    print("## Testing correctness of sandbag()")
 
-    data = data.T
+    print("")
+    print("# Testing algorithm on minimal data unjitted")
+    print("")
 
-    cats = {
-        "G1": [0, 2, 3, 8],
-        "S": [4, 5, 6],
-        "G2M": [1, 7, 9, 10]
+    settings.enable_jit = False
+
+    params = {
+        'data': min_ref_mat.T,
+        'annotation': min_ref_cats,
+        'gene_names': min_ref_gene_names,
+        'sample_names': min_ref_sample_names
     }
 
-    sample_names = list(string.ascii_uppercase[:12])
-    gene_names = list([str(x) for x in range(11)])
-
-    marker_pairs = pairs.sandbag(
-        data=data,
-        annotation=cats,
-        gene_names=gene_names,
-        sample_names=sample_names
-    )
+    marker_pairs = pairs.sandbag(**params)
 
     assert utils.same_marker(marker_pairs, min_ref)
 
-def test_sandbag_min_opt():
-    import string
+    utils.benchmark_test(pairs.sandbag, params)
 
-    data = np.array([
-        [48, 10, 63, 30, 1, 8, 4, 15, 20, 50, 20, 18],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [12, 42, 26, 8, 12, 1, 8, 38, 20, 15, 61, 52],
-        [22, 8, 8, 4, 43, 33, 21, 3, 1, 6, 4, 6],
-        [1, 41, 6, 5, 15, 12, 16, 8, 9, 3, 33, 29],
-        [30, 4, 41, 38, 3, 3, 8, 1, 0, 51, 6, 2],
-        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        [2, 31, 6, 5, 31, 26, 29, 19, 28, 13, 29, 31],
-        [38, 12, 12, 43, 0, 18, 1, 4, 6, 51, 8, 9],
-        [10, 18, 18, 11, 6, 1, 5, 21, 5, 14, 18, 16],
-        [99, 1, 87, 66, 18, 19, 11, 2, 19, 75, 2, 4]
-    ])
+    print("")
+    print("# Testing algorithm on minimal data jitted, single core")
+    print("")
 
-    data = data.T
-
-    cats = {
-        "G1": [0, 2, 3, 8],
-        "S": [4, 5, 6],
-        "G2M": [1, 7, 9, 10]
-    }
-
-    sample_names = list(string.ascii_uppercase[:12])
-    gene_names = list([str(x) for x in range(11)])
-
-    marker_pairs = pairs.sandbag(
-        data=data,
-        annotation=cats,
-        gene_names=gene_names,
-        sample_names=sample_names,
-        opt=True
-    )
-
-    assert utils.same_marker(marker_pairs, min_ref)
-
-def test_sandbag_1j():
+    settings.verbosity = 4
     settings.n_jobs = 1
+    settings.enable_jit = True
+
+    marker_pairs = pairs.sandbag(**params)
+
+    assert utils.same_marker(marker_pairs, min_ref)
+
+    utils.benchmark_test(pairs.sandbag, params)
+
+    print("")
+    print("# Testing algorithm on minimal data jitted, multi core")
+    print("")
+
+    settings.verbosity = 4
+    settings.n_jobs = 4
+    settings.enable_jit = True
+
+    marker_pairs = pairs.sandbag(**params)
+
+    assert utils.same_marker(marker_pairs, min_ref)
+
+    utils.benchmark_test(pairs.sandbag, params)
+
+
+def test_benchmark_sandbag():
+    print("")
+    print("")
+
+    print("## Testing speed of sandbag()")
+
+    print("")
+    print("# Testing on (50, 500) with single core, unjitted")
+    print("")
+
+    settings.n_jobs = 1
+    settings.enable_jit = False
+    utils.benchmark_test(pairs.sandbag, utils.generate_random_testdata(50, 500))
+
+    print("")
+    print("# Testing on (50, 500) with single core, jitted")
+    print("")
+
+    settings.n_jobs = 1
+    settings.enable_jit = True
+    utils.benchmark_test(pairs.sandbag, utils.generate_random_testdata(50, 500))
+
+    print("")
+    print("# Testing on (150, 1000) with single core, jitted")
+    print("")
+
+    settings.n_jobs = 1
+    utils.benchmark_test(pairs.sandbag, utils.generate_random_testdata(150, 1000))
+
+    print("")
+    print("# Testing on (150, 1000) with multi core, jitted")
+    print("")
+
+    settings.n_jobs = 4
+    utils.benchmark_test(pairs.sandbag, utils.generate_random_testdata(150, 1000))
+
+    print("")
+    print("# Testing on (250, 2500) with single core")
+    print("")
+
+    settings.n_jobs = 1
+    utils.benchmark_test(pairs.sandbag, utils.generate_random_testdata(250, 2500))
+
+    print("")
+    print("# Testing on (250, 2500) with multi core")
+    print("")
+
+    settings.n_jobs = 4
+    utils.benchmark_test(pairs.sandbag, utils.generate_random_testdata(250, 2500))
+
+def test_sandbag_inputs():
+    print("")
+    print("")
+
+    print("## Testing different input types for sandbag()")
+
+    settings.n_jobs = 4
+    settings.verbosity = 4
+
+    print("")
+    print("# Testing AnnData obj, including annotation")
+    print("")
 
     training_data = datasets.leng15(mode='sorted', gene_sub=list(range(0, 1000)))
+
     marker_pairs = pairs.sandbag(training_data)
 
-    if not utils.same_marker(marker_pairs, ref_markers):
-        raise AssertionError()
+    assert utils.same_marker(marker_pairs, ref_markers)
 
+    print("")
+    print("# Testing AnnData obj, with separate annotation")
+    print("")
 
-def test_sandbag_2j():
-    settings.n_jobs = 8
-    print("reg")
-
-    training_data = datasets.leng15(mode='sorted', gene_sub=list(range(0, 1000)))
-    marker_pairs = pairs.sandbag(training_data)
-
-    if not utils.same_marker(marker_pairs, ref_markers):
-        raise AssertionError()
-
-def test_sandbag_2j_opt():
-    settings.n_jobs = 8
-    print("opt")
-
-    training_data = datasets.leng15(mode='sorted', gene_sub=list(range(0, 1000)))
-    marker_pairs = pairs.sandbag(training_data, opt=True)
-
-    if not utils.same_marker(marker_pairs, ref_markers):
-        raise AssertionError()
-
-
-def test_sandbag_2j_ann():
-    settings.n_jobs = 2
-
-    training_data = datasets.leng15(mode='sorted', gene_sub=list(range(0, 1000)))
     annotation = {
         cat: [i for i, x in enumerate(training_data.obs['category']) if x == cat]
         for cat in ["G1", "S", "G2M"]
     }
 
-    print(annotation)
     marker_pairs = pairs.sandbag(training_data, annotation=annotation)
 
-    if not utils.same_marker(marker_pairs, ref_markers):
-        raise AssertionError()
+    assert utils.same_marker(marker_pairs, ref_markers)
 
+    print("")
+    print("# Testing DataFrame obj, with separate annotation")
+    print("")
 
-def test_sandbag_Allj_ann():
-    settings.n_jobs = 8
-
-    training_data = datasets.leng15(mode='sorted', gene_sub=list(range(0, 1000)))
-    annotation = {
-        cat: [i for i, x in enumerate(training_data.obs['category']) if x == cat]
-        for cat in ["G1", "S", "G2M"]
-    }
-
-    marker_pairs = pairs.sandbag(training_data, annotation=annotation)
-
-    if not utils.same_marker(marker_pairs, ref_markers):
-        raise AssertionError()
-
-
-def test_sandbag_df():
-    training_data = datasets.leng15(mode='sorted', gene_sub=list(range(0, 1000)))
     training_data_df = DataFrame(training_data.X)
 
     sample_names = list(training_data.obs_names)
@@ -183,35 +208,24 @@ def test_sandbag_df():
     training_data_df.Index = sample_names
     training_data_df.columns = gene_names
 
-    annotation = {
-        cat: [i for i, x in enumerate(training_data.obs['category']) if x == cat]
-        for cat in ["G1", "S", "G2M"]
-    }
+    marker_pairs = pairs.sandbag(training_data_df, annotation=annotation)
 
-    marker_pairs_df = pairs.sandbag(training_data_df, annotation)
+    assert utils.same_marker(marker_pairs, ref_markers)
 
-    if not utils.same_marker(marker_pairs_df, ref_markers):
-        raise AssertionError()
+    print("")
+    print("# Testing DataFrame obj, with separate annotation and separate gene-/sample_names")
+    print("")
 
-    marker_pairs_df = pairs.sandbag(training_data_df, annotation, gene_names, sample_names)
+    marker_pairs = pairs.sandbag(training_data_df, annotation, gene_names, sample_names)
 
-    if not utils.same_marker(marker_pairs_df, ref_markers):
-        raise AssertionError()
+    assert utils.same_marker(marker_pairs, ref_markers)
 
+    print("")
+    print("# Testing ndarray obj, with separate annotation and separate gene-/sample_names")
+    print("")
 
-def test_sandbag_np():
-    training_data = datasets.leng15(mode='sorted', gene_sub=list(range(0, 1000)))
-    training_data_df = DataFrame(training_data.X)
     training_data_np = training_data_df.values
 
-    sample_names = list(training_data.obs_names)
-    gene_names = list(training_data.var_names)
-    annotation = {
-        cat: [i for i, x in enumerate(training_data.obs['category']) if x == cat]
-        for cat in ["G1", "S", "G2M"]
-    }
+    marker_pairs = pairs.sandbag(training_data_np, annotation, gene_names, sample_names)
 
-    marker_pairs_np = pairs.sandbag(training_data_np, annotation, gene_names, sample_names)
-
-    if not utils.same_marker(marker_pairs_np, ref_markers):
-        raise AssertionError()
+    assert utils.same_marker(marker_pairs, ref_markers)
